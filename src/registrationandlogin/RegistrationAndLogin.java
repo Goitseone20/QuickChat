8c669188a6550449a9ac005ee48d404680f85526
Student <Student@LabVM2047644.wdzgkvdr0wee5iyl22zycuru3h.jnbx.internal.cloudapp.net>
2026-04-10 10:50:05 +0200

    added String UserName, Password , Cellphone

--- src/registrationandlogin/RegistrationAndLogin.java
+++ src/registrationandlogin/RegistrationAndLogin.java
@@ -10,18 +10,40 @@ import java.util.Scanner;
  */
 class Login{
     String UserName;
-    String password;
-    String cellphone;
+    String Password;
+    String Cellphone;
+    
      
     boolean checkUserName(String UserName){
-        if (UserName.contains() && UserName.length()== 5{
+        if (UserName.length()==5 && UserName.contains("_")){
+            System.out.println("UserName successfully captured");
             return true;
+        
         }
-   
-    
-    
+        
+        else{
+            System.out.println("UserName is not correctly formatted");
+            return false;
+            
+        }
+    boolean checkPassword(String Password){
+        if (Password.length()==8 && Password.contains(".*[A-Z].") && Password.contains("\\d") && Password.contains(".[!@#$%^&*].")){
+            System.out.println("Password successfully captured ");
+           return true; 
+        }
+        else{
+            System.out.println("Password is not correctly formatted");
+            return false;
+        }
+        
+    }
+    boolean checkCellphone
+     
+        
     }
 }
+       
+    
 public class RegistrationAndLogin {
 
     /**
